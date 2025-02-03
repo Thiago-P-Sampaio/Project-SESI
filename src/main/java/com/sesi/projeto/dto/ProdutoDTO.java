f0a5c80ac9c002b3abc17ef246aee3cb867d6fcd
Thiago P <thiago.p.sampaio07@gmail.com>
2025-02-03 16:38:47 -0300

    :sparkles: feat: Implementação dos DTO's

--- src/main/java/com/sesi/projeto/dto/ProdutoDTO.java
+++ src/main/java/com/sesi/projeto/dto/ProdutoDTO.java
@@ -1,5 +1,8 @@
 package com.sesi.projeto.dto;
 
-public record ProdutoDTO(String nome, double preco , String descricao) {
+public record ProdutoDTO(String nome,
+                         double preco ,
+                         String descricao,
+                         String imgUrl) {
 
 }
